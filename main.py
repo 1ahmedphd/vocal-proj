f9a73fcd15d9c80fa255ad44c17af48efe7f2801
1ahmedphd <50165569+1ahmedphd@users.noreply.github.com>
2024-08-25 09:43:43 +0300

    switched to fucking chromium

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from splinter import Browser
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 import socket
 import datetime
 import subprocess
@@ -17,13 +17,11 @@ args = parse.parse_args()
 
 tor_process = subprocess.Popen(["tor", "-f", f"insts/torrc_{args.itter}"])
 
-firefox_options = Options()
+chrome_options = Options()
 
 # Configure the proxy settings
-firefox_options.set_preference("network.proxy.type", 1)
-firefox_options.set_preference("network.proxy.socks", "127.0.0.1")
-firefox_options.set_preference("network.proxy.socks_port", args.sock)
-firefox_options.set_preference("network.proxy.socks_remote_dns", True)
+chrome_options.add_argument("--proxy-server=socks5://127.0.0.1:9150")
+chrome_options.add_argument("--headless")
 
 def is_tor_running(host='127.0.0.1', port=args.sock):
     try:
@@ -36,7 +34,7 @@ while not is_tor_running():
     print("tor not running")
     time.sleep(1)
 print("Tor is running.")
-with Browser('firefox', options=firefox_options,) as browser:
+with Browser('firefox', options=chrome_options) as browser:
     browser.visit('http://adfoc.us/8627851')
     if browser.is_element_present_by_xpath("/html/body/div[2]/div[1]/a/img", wait_time=10):
         pass
